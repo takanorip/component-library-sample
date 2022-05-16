99e608a08a62ec15e050a6f92e4feb0fccff220d
takanorip <takanori0170@gmail.com>
2022-05-16 18:33:23 +0900

    Update

--- src/Button/Button.test.tsx
+++ src/Button/Button.test.tsx
@@ -2,16 +2,10 @@ import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './Button.stories'
 
-const { Default, Outlined } = composeStories(stories);
+const { Default } = composeStories(stories);
 
 test('renders default button with default args', () => {
   render(<Default>Button</Default>);
-  const buttonElement = screen.getByRole('button');
-  expect(buttonElement).not.toHaveStyle({ color: 'white' });
-});
-
-test('renders default button with overriden props', () => {
-  render(<Default outlined>Button</Default>);
-  const buttonElement = screen.getByRole('button');
-  expect(buttonElement).not.toHaveStyle({ color: 'var(--color-blue-500)' });
+  const buttonElement = screen.getByText(/Button/i);
+  expect(buttonElement).not.toBeNull();
 });
